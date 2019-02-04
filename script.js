b55525e8087615bd25c5b2a3e6fc8d68ed130b19
mikebrown4000 <mikebrown4000@gmail.com>
2019-02-04 13:46:54 -0500

    pseudocoded make choice, making removeDivs

--- script.js
+++ script.js
@@ -13,11 +13,38 @@ console.log(choiceTwo);
 let newBackground = 'red';
 
 //assign event listenrs:
-choiceOne.addEventListener('click', changeBackground);
-choiceTwo.addEventListener('click', changeBackground);
+choiceOne.addEventListener('click', makeChoice);
+choiceOne.addEventListener('hover', changeBackground);
+choiceTwo.addEventListener('click', makeChoice);
+choiceTwo.addEventListener('hover', changeBackground);
 
 
-function changeBackground(newBackground){
- const bodyEl = document.getElementByTagName('body');
- body.style.background = newBackground;
+function makeChoice(){
+//assign background
+//remove divs()
+//remove (animated)
+//find text
+//display text (animated)
+//change gamestate (make new divs)
+
+  //give event listeners
+//display divs (animated)
+//await response
+
+};
+
+
+
+function changeBackground(){
+ const bodyEl = document.querySelector('body');
+ bodyEl.style.background = 'red';
+};
+
+//remove divs called in choice made and background change
+function removeDivs(){
+  const divParent = document.querySelector('main');
+  //go through main removing divs
+  while(divParent.firstChild){
+    divParent.removeChild(divParent.firstChild);
+  }
 };
