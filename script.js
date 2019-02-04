dac79f5d3c4a8fb3f6cbb3c21ba2ce92eb7ac0dd
mikebrown4000 <mikebrown4000@gmail.com>
2019-02-04 15:54:02 -0500

    integrated make new divs function

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ console.log('hi');
 //*********FLOW*********
 //1.PRESENT TEXT
 //2.PRESENT CHOICES
-//3.REMOVE CHOICES
-//4.REPEAT
+//3.REMOVE TEXT
+//4.REMOVE CHOICES
+//5.MOVE TO NEW AREA
+//6.REPEAT
 
 
 //FUNCTION CITY BAYBAY!!!!
@@ -14,10 +16,23 @@ function removeDivs(){
   const divParent = document.querySelector('section');
   console.log(divParent);
   while(divParent.firstChild){
+    //add to fade away CSS class?
     divParent.removeChild(divParent.firstChild);
   }
   console.log('passed while loop');
 };
+
+//make new divs function (very under construction)
+function makeDivs(){
+  console.log('entered make divs');
+  for (var i = 0; i < 3; i++) {
+  const newChoice = document.createElement('div');
+  newChoice.textContent = 'New Choice';
+  document.querySelector('section').appendChild(newChoice);
+  }
+};
+
+
 //changeBackground Declaration
 function changeBackground(){
   console.log('changeBackground called');
@@ -25,7 +40,11 @@ function changeBackground(){
  bodyEl.style.background = 'red';
 };
 
-function makeDivs(){
+
+//this is going to be a goddamn mess of a function..
+
+
+function loadNewWorld(){
 
   //get passed user decision and gamestate
     //// IDEA:  check choice counter?
@@ -35,11 +54,13 @@ function makeDivs(){
     //make giant divs,fuzzy divs, or sun divs
   //else make giant endings, fuzzy endings, or sun endings
     //
-}
+};
 
 
 
 
+
+//finds divs or chocies
 const choiceOne = document.getElementById('choiceOne');
 console.log(choiceOne);
 const choiceTwo = document.getElementById('choiceTwo');
@@ -64,7 +85,8 @@ console.log('passed removeDivs');
 //find text
 //display text (animated)
 //change gamestate (make new divs)
-
+makeDivs();
+console.log('passed makeDivs');
   //give event listeners
 //display divs (animated)
 //await response
